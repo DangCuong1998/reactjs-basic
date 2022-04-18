bdbbdd58d5077f4dee3323caf9d5679757f31fc9
DangCuong1998 <cuongdq62@wru.vn>
2022-04-18 11:15:13 +0700

    day bai event

--- src/views/example/MyComponent.js
+++ src/views/example/MyComponent.js
@@ -5,18 +5,29 @@ class MyComponent extends React.Component {
         name: 'Eric',
         channel: 'Hoi dan it'
     }
+    handleOnchangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+    handleClickButton = () => {
+        alert('click me');
+    }
     render() {
         let name = 'cuong'
         return (
             <div className='abc'>
                 <div className='first'>
+                    <input value={this.state.name} type="text" onChange={(event) => this.handleOnchangeName(event)} />
                     {console.log('My name is:', name)}
                     hello my Component Hoi dan it,My name is {this.state.name}
                 </div>
                 <div className='second'>
                     My youtube channel: {this.state.channel};
                 </div>
-
+                <div className='third'>
+                    <button onClick={() => this.handleClickButton()}>click me</button>
+                </div>
             </div>
 
         )
